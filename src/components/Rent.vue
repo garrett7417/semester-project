6ab0b43cf5008e02c525b3eb4087007818175e16
garrett7417 <43021370+garrett7417@users.noreply.github.com>
2021-04-16 22:46:08 -0400

    Update Rent.vue

--- src/components/Rent.vue
+++ src/components/Rent.vue
@@ -4,6 +4,9 @@
     <!-- <label>Group Size: </label><input type="number" max="15" placeholder="Enter number of adults" />
     <input type="number" max="15" placeholder="Enter number of children" /><br> -->
     <h3>Choose the boat(s) you want</h3><br>
+    <select v-model="boatName">
+        <option v-for="(c,pos) in allBoats" :value="c.name" :key="pos"> {{c.name}}</option>
+    </select>
   </div>
 </template>
 
@@ -11,9 +14,33 @@
 
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { FirebaseAuth, UserCredential } from "@firebase/auth-types";
+import { FirebaseFirestore, QueryDocumentSnapshot, QuerySnapshot } from 'node_modules/@firebase/firestore-types';
 
 @Component
 export default class Rent extends Vue{
+    readonly $appAuth!: FirebaseAuth;
+    readonly $appDB!: FirebaseFirestore;
+    private uid = "none";
+    private boatName = "Hi";
+    private allBoats: any[] = [];
+
+    mounted(): void{
+        this.uid = this.$appAuth.currentUser?.uid ?? "none";
+        this.$appDB
+            .collection(`WaterCrafts`)
+            .orderBy("name")
+            .onSnapshot ((qs: QuerySnapshot) =>{
+                this.allBoats.splice(0);
+                qs.forEach((qds: QueryDocumentSnapshot) => {
+                    if(qds.exists && this.boatName != undefined){
+                        const boatName = qds.data();
+                        this.allBoats.push({
+                            name: boatName.name
+                        })
+                    }
+                })
+            })
+    }
   
 }
 
