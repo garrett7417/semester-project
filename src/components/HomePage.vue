88aacb14fdad23f179d4d30855c63ef4e667ef4d
Chase Baker <79476382+bakechas@users.noreply.github.com>
2021-04-19 23:51:55 -0400

    Changed fav icon and background

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -18,28 +18,29 @@
           <div class="menu-toggle"><i class="fa fa-bars" aria-hidden="true"></i></div>
       </header>
     </div>
-    
-    <div class="gallary" align="center">
-      <p>Your boating adventure starts here!</p>
-      <h1>Boat Gallary</h1>
-      <div class="thumbnails">
-        <!-- Powerboat Images -->
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Malibu Wakesetter.jpg" alt="2021 Malibu Wakesetter">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/MasterCraft x26.jpeg" alt="2021 MasterCraft X26">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Supra.jpg" alt="2021 Supra">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2020 Pavati AL26.jpg" alt="2020 Pavati AL26">
-        <!-- Pontoon Images -->
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2018 Manitou Pontoon.jpg" alt="2018 Manitou Pontoon">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2020 Bennington Pontoon.jpg" alt="2020 Bennington Pontoon">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Manitou Tri-toon.jpg" alt="2021 Manitou Tri-toon">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2019 Bennington Tri-toon.jpg" alt="2019 Bennington Tri-toon">
-        <!-- Waverunner Images -->
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Seadoo Spark Trixx.jpg" alt="2021 Seadoo Spark Trixx">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Seadoo Spark.jpg" alt="2020 Seadoo Spark">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Yamaha Waverunner1.jpg" alt="2021 Yamaha Waverunner">
-        <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Yama Waverunner2.jpg" alt="2021 Yamaha Waverunner">
+    <div class="backgroundImg" style="background-image: url('../assets/mapOfLake2.jpg');">
+      <div class="gallary" align="center">
+        <p>Your boating adventure starts here!</p>
+        <h1>Boat Gallary</h1>
+        <div class="thumbnails">
+          <!-- Powerboat Images -->
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Malibu Wakesetter.jpg" alt="2021 Malibu Wakesetter">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/MasterCraft x26.jpeg" alt="2021 MasterCraft X26">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Supra.jpg" alt="2021 Supra">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2020 Pavati AL26.jpg" alt="2020 Pavati AL26">
+          <!-- Pontoon Images -->
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2018 Manitou Pontoon.jpg" alt="2018 Manitou Pontoon">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2020 Bennington Pontoon.jpg" alt="2020 Bennington Pontoon">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Manitou Tri-toon.jpg" alt="2021 Manitou Tri-toon">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2019 Bennington Tri-toon.jpg" alt="2019 Bennington Tri-toon">
+          <!-- Waverunner Images -->
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Seadoo Spark Trixx.jpg" alt="2021 Seadoo Spark Trixx">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Seadoo Spark.jpg" alt="2020 Seadoo Spark">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Yamaha Waverunner1.jpg" alt="2021 Yamaha Waverunner">
+          <img onmouseover="getElementById('preview').src=this.src" src="../assets/2021 Yama Waverunner2.jpg" alt="2021 Yamaha Waverunner">
 
-      </div>
+        </div>
+      </div> <!--background image div-->
       <div class="preview" align="center">
         <img id="preview" src="../assets/2021 Malibu Wakesetter.jpg" alt="2021 Malibu Wakesetter">
       </div>
